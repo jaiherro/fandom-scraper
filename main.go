8cb43956c1b7e7ae265a6566879c1bcc9e490979
Jai <36728057+JaiHerro@users.noreply.github.com>
2020-04-10 02:50:54 +1000

    work

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
 func main() {
-	
+	// Instantiate default collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("godoc.org"),
+		colly.AllowURLRevisit(),
+		colly.Async(true),
+	)
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("table.wikitable tbody tr td b a", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if strings.HasSuffix(link, "jpg") || strings.HasSuffix(link, "png") {
+			download(link)
+		}
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://simpsons.fandom.com/wiki/List_of_Episodes/")
+}
+
+func download(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(ioutil.ReadAll(resp.Body))
 }
